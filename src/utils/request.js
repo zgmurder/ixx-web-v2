e0140b07e0ad240fe71fd298bd6a29aebc4fb1fa
zgmurder <294069733@qq.com>
2019-12-06 14:45:23 +0800

    api

--- src/utils/request.js
+++ src/utils/request.js
@@ -59,24 +59,29 @@ service.interceptors.response.use(
     const res = response.data
     // if the custom code is not 20000, it is judged as an error.
     if (res.code !== 200 && res.code !== 0) {
-      if (res.code === 401) {
-        Message({
-          message: '登录信息失效,准备退出登录...',
-          type: 'warning',
-          duration: 3 * 1000
-        })
-        setTimeout(() => {
-          store.dispatch('loginout')
-        }, 3000)
-      } else {
-        if (res.message !== null) {
-          Message({
-            message: res.message || 'Server Error',
-            type: 'error',
-            duration: 5 * 1000
-          })
-        }
-      }
+      // if (res.code === 401) {
+      //   Message({
+      //     message: '登录信息失效,准备退出登录...',
+      //     type: 'warning',
+      //     duration: 3 * 1000
+      //   })
+      //   setTimeout(() => {
+      //     store.dispatch('loginout')
+      //   }, 3000)
+      // } else {
+      //   if (res.message !== null) {
+      //     Message({
+      //       message: res.message || 'Server Error',
+      //       type: 'error',
+      //       duration: 5 * 1000
+      //     })
+      //   }
+      // }
+      Message({
+        message: res.message || 'Server Error',
+        type: 'error',
+        duration: 5 * 1000
+      })
       // 50008: Illegal token; 50012: Other clients logged in; 50014: Token expired;
       // if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
       //   // to re-login
